178f7a463e85b8bb6c5e011bab4534b0084d7267
nickzaf95 <nicholaszafiriou@gmail.com>
2020-04-07 15:52:35 +0200

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Add your functions here
+const map = (array, func) => {
+    let arr = []
+    array.forEach(element => {
+        arr.push(func(element))
+    });
+    return arr
+}
+
+const reduce = (array, func, startingPoint=0) => {
+    let result = startingPoint
+    if (typeof (func(result, array[0])) === "boolean") {
+        result = true
+    }
+    array.forEach(element => {
+        result = func(result, element)
+    });
+    return result
+}
